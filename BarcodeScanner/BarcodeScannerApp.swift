33442769f1006b60541f37bca973738c1e284e01
HassenYahia <hassenabdurahman4047@gmail.com>
2023-10-04 22:29:14 -0400

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Hassen Abdurahman Yahia on 2023-10-04.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
